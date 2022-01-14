6b870c98e133cb1b1a47e6dbe5f7598a7e5677b6
sergiodrm <sergiojg.drm@gmail.com>
2022-01-14 02:17:13 +0100

    Added custom GameState

---
+++ Source/MultiplayerShooter/Game/MultiplayerShooterGameState.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/GameState.h"
+
+#include "MultiplayerShooterGameState.generated.h"
+
+UCLASS()
+class AMultiplayerShooterGameState : public AGameState
+{
+    GENERATED_BODY()
+};
